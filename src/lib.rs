e632c26907f9440652efaaeac6d6792e647bfd64
Corey Farwell <coreyf@rwell.org>
2014-12-12 19:15:12 -0500

    Move TODO to GitHub issues

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 // Many of my decisions were based off the following document
 // https://web.archive.org/web/20140727185616/http://algs4.cs.princeton.edu/33balanced/
 
-
-// TODO:
-// Collection trait
-// convert this to Key/Value K/V
-
 use Direction::{Left, Middle, Right, Leaf};
 use InsertResult::{Fit, Split};
 
